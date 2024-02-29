24c84167f58c48761a6df6dbae89cdc243eee924
thadeu <guilherme.thadeu@qodeless.io>
2024-02-28 21:53:35 -0300

    first commit

---
+++ drizzle/0001_first_silver_centurion.sql
@@ -0,0 +1,6 @@
+CREATE TABLE IF NOT EXISTS "restaurants" (
+	"id" text PRIMARY KEY NOT NULL,
+	"name" text NOT NULL,
+	"created_at" timestamp DEFAULT now() NOT NULL,
+	"updated_at" timestamp DEFAULT now() NOT NULL
+);
